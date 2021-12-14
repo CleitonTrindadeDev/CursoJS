c5409a8ea1f9689b688b809f7ddcf6f1f641e9af
Cleiton Trindade <93351789+CleitonTrindadeDev@users.noreply.github.com>
2021-12-13 23:35:32 -0300

    Condições em JS

---
+++ Aula11/EX008.js
@@ -0,0 +1,7 @@
+console.log('console funcionando ')//escrever na tela no node.js aperta f8//
+
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
