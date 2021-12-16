dcc28fa78d2ea4b4a24c386ef30acb545054d251
Cleiton Trindade <93351789+CleitonTrindadeDev@users.noreply.github.com>
2021-12-15 22:45:40 -0300

    Repetição em JS

---
+++ Aula13/ambiente copy.js
@@ -0,0 +1,6 @@
+//teste logico no final//
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+}while (c <= 15) 
---
+++ Aula13/ambiente.js
@@ -0,0 +1,6 @@
+//teste logico no inicio//
+var c = 1
+while (c <= 10) {
+    console.log(`passo ${c}`)
+    c++
+}
